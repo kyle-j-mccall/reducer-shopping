3de01d096109eb59ba09f8871f954ba4df5cf04d
Kyle McCall <kylem147@gmail.com>
2023-01-13 16:52:35 -0800

    update dom test passing

--- src/components/reducers/shopping-list-item-reducer.test.js
+++ src/components/reducers/shopping-list-item-reducer.test.js
@@ -6,14 +6,12 @@ import {
 import { createPost } from '../../actions/posts';
 
 describe('shopping-list-item-reducer', () => {
-  it('post load', () => {
-    it('DOM is updated when new items are added', () => {
-      const prevState = {
-        ...initialState(),
-        newPost: 'test post',
-      };
-      const newState = reducer(prevState, createPost('expect me'));
-      expect(newState.newPost).toEqual('expect me');
-    });
+  it('DOM is updated when new items are added', () => {
+    const prevState = {
+      ...initialState(),
+      postFormValue: 'test post',
+    };
+    const newState = reducer(prevState, createPost('expect me'));
+    expect(newState.postFormValue).toEqual('expect me');
   });
 });
